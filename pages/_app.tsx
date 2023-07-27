6b8352ff8fed9c86f400c6033b8e1f7cfaa26ddf
dev0_gb97 <dev0_gb97@outlook.com>
2023-07-27 11:55:30 -0500

    Bienvenida

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import Layout from "@/components/layout/Layout";
 import "../styles/scss/styles.scss";
 import { AppProps } from "next/app";
+import { AuthProvider } from "@/Context/AuthContext";
 
 function MyApp ({Component, pageProps}: AppProps){
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Sidebar from "@/components/Sidebar";
 import { useState } from "react";
+import { Col, Container, Row } from "reactstrap";
 
 export default function Home() {
   
@@ -11,6 +12,30 @@ export default function Home() {
         <meta name="description" content="Creado por GB97 Ecuador"/> 
         <link rel="icon" href="/logo.ico"/>
       </Head>
-    </div>
+      
+      <h1 className="text-center pt-3">Gestión de Empleados</h1>
+
+      <div className="container-inicio">
+        <h3 className="text-center">Bienvenido a la pagina Gestion de Empleados</h3>
+        <h4 className="text-center">En esta página podra realizar los siguiente</h4>
+
+        <Row className="row-container">
+          <Col md='6'>
+            <Container className="custom-container">
+              <i className="fa fa-user-plus" />
+              <h4 className="text-center">Creación de Empleados</h4>
+              <p className="text-center">Permite registrar un nuevo empleado en el sistema</p>
+            </Container>
+          </Col>
+          <Col md='6'>
+            <Container className="custom-container">
+              <i className="fa fa-bars" />
+              <h4 className="text-center">Listar Empleados</h4>
+              <p className="text-center">Permite listar, modificar y eliminar un empleado del sistema</p>
+            </Container>
+          </Col>
+        </Row>
+      </div>
+      </div>
   )
 }
